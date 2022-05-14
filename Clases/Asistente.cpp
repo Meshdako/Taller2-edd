31cda66e1029f18cf7098701d1d53302eefd4f0e
Meshdako <msobarzo@utem.cl>
2022-05-14 13:22:42 -0400

    Agregamos la clase asistente

---
+++ Clases/Asistente.cpp
@@ -0,0 +1,32 @@
+#include "Asistente.h"
+#include <iostream>
+
+using namespace std;
+
+Asistente::Asistente()
+{
+	tipoEntrada = 0;
+	ocupada = 0;
+}
+
+Asistente::Asistente(string a, RUT b, Direccion c, Fecha d, int e, Fecha f, bool g) : Persona(a, b, c, d)
+{
+	tipoEntrada = e;
+	fechaEvento = f;
+	ocupada = g;
+}
+
+void Asistente::verAsistente()
+{
+	verPersona();
+	cout << "Tipo de entrada: " << tipoEntrada << endl;
+	cout << "Fecha del evento: ";
+	fechaEvento.verFecha();
+	cout << "Ocupada: ";
+
+	if(ocupada == 0)
+		cout << "No" << endl;
+	else
+		cout << "Si" << endl;
+}
+
---
+++ Clases/Asistente.h
@@ -0,0 +1,29 @@
+#ifndef ASISTENTE_H
+#define ASISTENTE_H
+
+#include <iostream>
+
+using namespace std;
+
+class Asistente : public Persona
+{
+	private:
+		int tipoEntrada;
+		Fecha fechaEvento;
+		bool ocupada;
+	public:
+		Asistente();
+		Asistente(string, RUT, Direccion, Fecha, int, Fecha, bool);
+
+		void setTipoEntrada(int x){tipoEntrada = x;}
+		void setFechaEvento(Fecha x){fechaEvento = x;}
+		void setOcupada(int x){ocupada = x;}
+
+		int getTipoEntrada(){return tipoEntrada;}
+		Fecha getFechaEvento(){return fechaEvento;}
+		bool getOcupada(){return ocupada;}
+
+		void verAsistente();
+}
+
+#endif //ASISTENTE_H
